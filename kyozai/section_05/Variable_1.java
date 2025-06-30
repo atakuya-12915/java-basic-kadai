dc67dbd2648a7b09db1205cf5fc9443333226ea5
atakuya-12915 <t.taku0402@outlook.com>
2025-06-30 22:45:33 +0900

    kyozai section_05追加

---
+++ kyozai/section_05/Variable_1.java
@@ -0,0 +1,18 @@
+package text.section_05;
+
+public class Variable_1 {
+	public static void main(String[] args) {
+		
+		// 変数testを宣言
+		int test = 123;
+		
+		// 変数testの値を表示
+		System.out.println(test);
+	}
+}
+
+// Javaでは、変数を入れる箱の「型」と「変数名」の両方を定義する必要があります！
+	// 型の定義は、[データ型] [変数名];　のように書きます。 → int test;
+	// 変数の宣言時にデータを代入することも可能です。これを[初期化]と呼びます。
+	//[データ型] [変数名] = [変数の初期値]; のように書きます。 → int test = 123;
+	
