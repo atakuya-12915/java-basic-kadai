7ef484646a1890bdd2da64648a7a0ac398296cfd
atakuya-12915 <t.taku0402@outlook.com>
2025-07-06 23:17:21 +0900

    template add

---
+++ template/Main_class.java
@@ -0,0 +1,17 @@
+package template;
+// 実行クラスのテンプレート用
+public class Main_class {
+	public static void main(String[] args) {
+		
+		// ここに、new オブジェクト生成（インスタンス化）を記述
+		  // objを生成することで、その設計図クラスにアクセスできるようになる
+		  	// ⚠️：[new 設計図クラス名] で生成　new_obj=設計図から作られたモノ
+		
+		
+		// ここに、メソッドの呼び出しを記述
+			// インスタンス名.メソッド名();		⚠️：staticなし
+			// クラス名.メソッド名();			⚠️：staticあり
+		
+	}
+
+}
