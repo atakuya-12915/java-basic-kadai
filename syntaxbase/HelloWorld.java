0dc8a32c02dc655c17426c8ce8b8b242c01983f9
atakuya-12915 <t.taku0402@outlook.com>
2025-07-07 12:38:22 +0900

    syntaxbase 追加

---
+++ syntaxbase/HelloWorld.java
@@ -0,0 +1,12 @@
+package syntaxbase;
+
+public class HelloWorld { 
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// Hello Worldと表示
+		System.out.println("Hello World");
+		System.out.println("こんにちは");
+	}
+
+}
