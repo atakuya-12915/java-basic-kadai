24b0550b98a110ccaf3d47f39a62f166f185962d
atakuya-12915 <t.taku0402@outlook.com>
2025-07-09 09:17:28 +0900

    ShowData 追加

---
+++ ShowData/ShowData.java
@@ -0,0 +1,20 @@
+package ShowData;
+
+public class ShowData {
+
+	public static void main(String[] args) {
+		// 変数の宣言と代入
+		int num = 10;
+		double pi = 3.14;
+		char blood  = 'A';
+		boolean fun = true;
+		String msg = "Hello";
+		
+		// 値の表示
+		System.out.println(num);
+		System.out.println(pi);
+		System.out.println(blood);
+		System.out.println(fun);
+		System.out.println(msg);
+	}
+}
